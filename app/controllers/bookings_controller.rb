f86258299c860e55b075623a80bfc20d62d3cfdc
Maurizio Ghisolfi <maurizioghisolfi93@gmail.com>
2020-11-11 12:30:04 +0100

    wip

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,23 @@
 class BookingsController < ApplicationController
   def show
+    # fetch bookings from db
+    # send them to bookings view for display -> @view.
   end
 
   def new
+    #
   end
 
   def create
+    # get time slot, user_id and hobby_id from the view
+    # create a new booking -> Booking.new(time_slot, user_id, hobby_id)
+    # Add to db
   end
 
   def edit
+    # get time slot, user_id and hobby_id from the view
+    # update the booking -> Booking.....(time_slot, user_id, hobby_id)
+    # Add to db
   end
 
   def update
